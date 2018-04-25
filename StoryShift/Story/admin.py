d5bd9d056814ba75cfe68cbcac77bdb9eefc75e9
Christopher-Arnold <christopher.cyan.arnold@gmail.com>
2018-04-25 14:21:06 +0000

    Story/Au's and Lccation Models

--- StoryShift/Story/admin.py
+++ StoryShift/Story/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Story, Location
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('id' , 'name')
+
+admin.site.register(Story, StoryAdmin)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id' , 'name')
+
+admin.site.register(Location, LocationAdmin)
--- StoryShift/Story/models.py
+++ StoryShift/Story/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Story(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter AU's Name")
+    
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter Location Name")
+    story = models.ForeignKey('Story', on_delete=models.SET_NULL, null=True)
